ac4ceb4fc63b832ef974de0ba5f38c6cd8279371
Mirko Iuliano <federico.iuliano@davinci.edu.ar>
2024-12-01 20:52:24 -0300

    mejore lo de los btnes de editar y ediar foto porque si los pongo en [ProfileData.js] se ven los botones hasta cuando estoy en el perfil de otra persona que no sea yo. Quizá en un futuro, para mejorar la estetica, me conviene poner un condicional que segun qué ruta está aparecen o no

--- src/pages/MyProfile.vue
+++ src/pages/MyProfile.vue
@@ -58,7 +58,17 @@ onUnmounted(() => {
 </script>
 
 <template>
-
+    <div class="flex items-end gap-4">
+        <h2 class="text-3xl text-center text-slate-800 font-bold my-6">Mi Perfil</h2>
+        <router-link 
+            to="/mi-perfil/editar"
+            class="mb-4 text-blue-700 underline"
+        >Editar</router-link>
+        <router-link 
+            to="/mi-perfil/editar/foto"
+            class="mb-4 text-blue-700 underline"
+        >Editar foto</router-link>
+    </div>
     <ProfileData :user="loggedUser"/>
 
     <PostCard 
