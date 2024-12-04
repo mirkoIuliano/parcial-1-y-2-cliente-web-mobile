62104ee72e97cb71e94441c6068853d958d8848a
Mirko Iuliano <federico.iuliano@davinci.edu.ar>
2024-12-04 08:12:41 -0300

    cree Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,49 @@
 <script setup>
-
+import BaseHeading from '../components/BaseHeading.vue';
 </script>
 
 <template>
-        Hola
+    <section class="my-16">
+        <div class="text-center max-w-3xl mx-auto">
+            <BaseHeading class="text-5xl mb-6"> Bienvenido a <span class="text-cyan-800">Coppermind</span> </BaseHeading>
+
+            <p class="text-lg text-slate-600">
+                Conecta con otros amantes de la lectura, comparte reseñas de tus libros favoritos, y descubre nuevas
+                historias para leer
+            </p>
+
+            <router-link 
+                to="/publicaciones" 
+                class="mt-6 inline-block bg-cyan-700 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-cyan-800 focus:bg-cyan-800 transition-colors"
+            > Ver Publicaciones </router-link>
+        </div>
+    </section>
+
+    <section class="py-16 px-8">
+        <BaseHeading>¿Qué puedes hacer en Coppermind?</BaseHeading>
+        
+        <div class="flex flex-wrap justify-center gap-10">
+            <div class="max-w-xs text-center">
+                <h3 class="text-xl font-semibold text-cyan-950 mb-2">Publica Reseñas</h3>
+                <p class="text-slate-600">
+                    Escribe reseñas de los libros que has leído y compártelas con la comunidad
+                </p>
+            </div>
+
+            <div class="max-w-xs text-center">
+                <h3 class="text-xl font-semibold text-cyan-950 mb-2">Chat Privado</h3>
+                <p class="text-slate-600">
+                    Conéctate con otros lectores a través del chat privado y comparte recomendaciones
+                </p>
+            </div>
+
+            <div class="max-w-xs text-center">
+                <h3 class="text-xl font-semibold text-cyan-950 mb-2">Personaliza tu Perfil</h3>
+                <p class="text-slate-600">
+                    Crea tu perfil y muestra tus últimas publicaciones
+                </p>
+            </div>
+        </div>
+    </section>
+
 </template>
